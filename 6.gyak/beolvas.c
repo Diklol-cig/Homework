064a403f9fbeb5a1c3fd3b330dec3f5f895da90d
Diklol-cig <63556954+Diklol-cig@users.noreply.github.com>
2022-03-16 11:40:26 +0100

    Add files via upload

--- 6.gyak/beolvas.c
+++ 6.gyak/beolvas.c
@@ -1,6 +1,7 @@
 //beolvasas prog1.h nelkul
 
 #include <stdio.h>
+#include <string.h>
 
 #define SIZE 200
 
@@ -10,6 +11,7 @@ int main()
     printf("Mi a neved köcsög? ");
 
     fgets(text, SIZE, stdin);
+    text[strlen(text)-1]= '\0';
 
     printf("Hello %s!\n", text);
     return 0;
